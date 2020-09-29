125dcacc3b62c88891109f28383f31d05f567f1b
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-28 22:26:44 -0300

    Inclusao do comportamento da exibiçao do curriculum

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
+window.onload = () => {
+  const fullCurriculum = document.querySelector('.full-curriculum');
+  fullCurriculum.style.display = 'none';
+}
+
 window.addEventListener('scroll', () => {
   const miniData = document.querySelector('.mini-data');
+  const fullCurriculum = document.querySelector('.full-curriculum');
 
-  if (window.scrollY >= 300) {
-    miniData.style.display = 'block';
+  if (fullCurriculum.style.display === 'none') {
+    if (window.scrollY >= 300) {
+      miniData.style.display = 'block';
+    } else {
+      miniData.style.display = 'none';
+    }
   } else {
-    miniData.style.display = 'none';
+      miniData.style.display = 'block';
   }
+}); 
+
+const toggleCurriculumDisplay = (toShow, toHide) => {
+  const miniData = document.querySelector('.mini-data');
+  const mainPhoto = document.querySelector('.main-photo');
+  const fullCurriculum = document.querySelector('.full-curriculum');
+  const closeBtn = document.querySelector('.full-curriculum-close');
+
+  miniData.style.display = toShow;
+  fullCurriculum.style.display = toShow;
+  closeBtn.style.display = toShow;
+  mainPhoto.style.display = toHide;
+}
+
+const curriculumBtn = document.querySelector('.info-item');
+
+curriculumBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  toggleCurriculumDisplay('inLine', 'none');
+});
+
+const closeBtn = document.querySelector('.full-curriculum-close');
+
+closeBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  toggleCurriculumDisplay('none', 'inLine');
 });
